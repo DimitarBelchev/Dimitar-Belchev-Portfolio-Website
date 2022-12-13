bf0caa858da28bb4af86f65bc2f706dbbce2c7ed
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-12-13 19:48:13 +0200

    setting up backend

---
+++ config/dbConfig.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect(process.env.mongo_url,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  // useCreateIndex:true
+});
+
+const connection = mongoose.connection;
+
+connection.on("error", () => {
+  console.log("Error connecting to database");
+});
+
+connection.on("connected", () => {
+  console.log("Mongo DB Connection Successful");
+});
+
+module.exports = connection;
